00b24a7a73007bd9c87b397178cb18171a4ef2ff
Neha R <137432377+Neharavi262001@users.noreply.github.com>
2024-02-09 21:53:23 +0530

    Update generateToken.js
    changed the generate token util function

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -1,16 +1,11 @@
 const jwt =require('jsonwebtoken')
 
 
-const generateToken=(req,res,userId)=>{
+const generateToken=(userId)=>{
 
     const token =jwt.sign({userId},process.env.SECRET_TOKEN,{expiresIn:'10d'})
     
-    res.cookie('jwt',token,{
-        httpOnly:true,
-        secure:true,
-        maxAge:10*24*60*60*1000
-
-    })
+   
     return token;
     
 }
