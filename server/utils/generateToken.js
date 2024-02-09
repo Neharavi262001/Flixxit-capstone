b23db77e4354184d62f7d0bbd670f4974e117137
Neharavi262001 <neharavi262001@gmail.com>
2024-02-09 21:14:28 +0530

    .

--- server/utils/generateToken.js
+++ server/utils/generateToken.js
@@ -5,13 +5,13 @@ const generateToken=(res,userId)=>{
 
     const token =jwt.sign({userId},process.env.SECRET_TOKEN,{expiresIn:'10d'})
     
-    // res.cookie('jwt',token,{
-    //     httpOnly:true,
-    //     secure:process.env.NODE_ENV !== 'development',
-    //     sameSite:'strict',
-    //     maxAge:10*24*60*60*1000
+    res.cookie('jwt',token,{
+        httpOnly:true,
+        secure:process.env.NODE_ENV !== 'development',
+        sameSite:'strict',
+        maxAge:10*24*60*60*1000
 
-    // })
+    })
     return token;
     
 }
