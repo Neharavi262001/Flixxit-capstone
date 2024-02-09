d2e88858eee15b32b42330e5f0f22d8bb2087fea
Neharavi262001 <neharavi262001@gmail.com>
2024-02-09 19:34:18 +0530

    .

--- server/middlewares/authenticationMiddleware.js
+++ server/middlewares/authenticationMiddleware.js
@@ -2,25 +2,42 @@ const jwt =require('jsonwebtoken')
 const asyncHandler=require('express-async-handler')
 const User =require('../models/userModel')
 
-const protectedRoutes=asyncHandler(async(req,res,next)=>{
-    const token = req.cookies.jwt;
+// const protectedRoutes=asyncHandler(async(req,res,next)=>{
+//     const token = req.cookies.jwt;
    
-    if (token){
-        try {
-          const decoded=jwt.verify(token,process.env.SECRET_TOKEN) 
+//     if (token){
+//         try {
+//           const decoded=jwt.verify(token,process.env.SECRET_TOKEN) 
         
-          req.user=await User.findById(decoded.userId).select('-password') 
-          next()
-        } catch (error) {
+//           req.user=await User.findById(decoded.userId).select('-password') 
+//           next()
+//         } catch (error) {
           
+//           res.status(401).json({ error: 'Not authorized, invalid token' });
+//         }
+
+//     }else{
+//         res.status(401)
+//         throw new Error('Not authorized , no token received')
+//     }
+
+// })
+
+const protectedRoutes = asyncHandler(async (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader && authHeader.startsWith('Bearer ')) {
+      const token = authHeader.split(' ')[1]; 
+
+      try {
+          const decoded = jwt.verify(token, process.env.SECRET_TOKEN);
+          req.user = await User.findById(decoded.userId).select('-password');
+          next();
+      } catch (error) {
           res.status(401).json({ error: 'Not authorized, invalid token' });
-        }
-
-    }else{
-        res.status(401)
-        throw new Error('Not authorized , no token received')
-    }
-
-})
-
+      }
+  } else {
+      res.status(401).json({ error: 'Not authorized, no token received' });
+  }
+});
 module.exports={protectedRoutes}
