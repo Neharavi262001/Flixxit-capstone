a482b86e147970d3781a9cde751e0c3a3ef91557
Neharavi262001 <neharavi262001@gmail.com>
2024-02-09 16:00:29 +0530

    .

--- server/config/db.js
+++ server/config/db.js
@@ -2,7 +2,7 @@ const mongoose =require('mongoose')
 
 const db=async()=>{
     try {
-        const connection =await mongoose.connect(process.env.MONGO_URI)
+        const connection =await mongoose.connect(process.env.MONGODB_URI)
         console.log(`Connected to Database : ${connection.connection.host}`)
     } catch (error) {
         console.error(`Error:${error.message}`)
