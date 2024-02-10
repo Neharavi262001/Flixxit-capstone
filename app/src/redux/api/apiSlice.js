d2a7e8cb2a9a9e02e8acefa528642709456c96e3
Neharavi262001 <neharavi262001@gmail.com>
2024-02-10 17:29:53 +0530

    cookie changes

--- app/src/redux/api/apiSlice.js
+++ app/src/redux/api/apiSlice.js
@@ -1,5 +1,6 @@
 import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
+
 const baseQuery=fetchBaseQuery({
     baseUrl:'https://flixxit-backend-x6u9.onrender.com',
     credentials:'include',
--- app/src/redux/auth/authSlice.js
+++ app/src/redux/auth/authSlice.js
@@ -4,6 +4,7 @@ const initialState={
     userInfo:localStorage.getItem('userInfo')
     ? JSON.parse(localStorage.getItem('userInfo'))
     : null,
+    token: localStorage.getItem("token") || null,
     
 }
 
@@ -13,11 +14,15 @@ const authSlice=createSlice({
     reducers:{
         setCredentials:(state,action)=>{
             state.userInfo=action.payload
+            state.token = action.payload
             localStorage.setItem('userInfo',JSON.stringify(action.payload))
+            localStorage.setItem("token", action.payload.token);
         },
         clearCredentials:(state,action)=>{
             state.userInfo=null,
+            state.token = null
             localStorage.removeItem('userInfo')
+            localStorage.removeItem("token");
         },
     }
 })
