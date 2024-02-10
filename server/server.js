f1507ace9523ee56fc6f0567db56292696efd68d
Neharavi262001 <neharavi262001@gmail.com>
2024-02-11 01:04:10 +0530

    .

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const PORT= process.env.PORT || 8000
 db()
 
 const corsOptions={
-    origin: 'https://flixxit-app-jfps.onrender.com/', 
+    origin: 'https://flixxit-app-jfps.onrender.com', 
     credentials: true 
 }
 
