d865bb4f0cb33340152d6aa85fa6ded5d26d0775
Neharavi262001 <neharavi262001@gmail.com>
2024-02-10 11:29:02 +0530

    cors setup

--- server/server.js
+++ server/server.js
@@ -10,10 +10,15 @@ const app=express()
 const PORT= process.env.PORT || 8000
 db()
 
+const corsOptions={
+    origin: 'http://localhost:3000', 
+    credentials: true 
+}
+
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
-app.use(cors())
+app.use(cors(corsOptions))
 
 app.use('/api/user',userRoutes)
 
