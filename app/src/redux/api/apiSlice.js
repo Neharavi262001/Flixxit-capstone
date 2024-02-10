1acf1097891123a24c4255182ccfa2e4c8deb0c3
Neharavi262001 <neharavi262001@gmail.com>
2024-02-10 18:12:36 +0530

    .

--- app/src/redux/api/apiSlice.js
+++ app/src/redux/api/apiSlice.js
@@ -4,13 +4,7 @@ import {createApi,fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 const baseQuery=fetchBaseQuery({
     baseUrl:'https://flixxit-backend-x6u9.onrender.com',
     credentials:'include',
-    // prepareHeaders: (headers, { getState }) => {
-    //     const token = getState().auth.token
-    //     if (token) {
-    //         headers.set("authorization", `Bearer ${token}`)
-    //     }
-    //     return headers
-    // }
+   
 })
 
 export const apiSlice=createApi({
--- app/src/redux/auth/authSlice.js
+++ app/src/redux/auth/authSlice.js
@@ -4,7 +4,7 @@ const initialState={
     userInfo:localStorage.getItem('userInfo')
     ? JSON.parse(localStorage.getItem('userInfo'))
     : null,
-    //token: localStorage.getItem("token") || null,
+    
     
 }
 
@@ -16,13 +16,13 @@ const authSlice=createSlice({
             state.userInfo=action.payload
             state.token = action.payload
             localStorage.setItem('userInfo',JSON.stringify(action.payload))
-            //localStorage.setItem("token", action.payload.token);
+          
         },
         clearCredentials:(state,action)=>{
             state.userInfo=null,
             state.token = null
             localStorage.removeItem('userInfo')
-            //localStorage.removeItem("token");
+            
         },
     }
 })
