2da6174a1a3187aee231961d4e7bc3d17abf5d4e
Neharavi262001 <neharavi262001@gmail.com>
2024-02-11 11:38:50 +0530

    .

--- server/server.js
+++ server/server.js
@@ -15,10 +15,12 @@ const corsOptions={
     credentials: true 
 }
 
+
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
-app.use(cors(corsOptions))
+
 
 app.use('/api/user',userRoutes)
 
